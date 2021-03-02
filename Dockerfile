9954449f9a0190d88998d60e9360bb867140cea0
Ben <me@bpmct.net>
2021-03-01 23:28:37 -0500

    fix permissions

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ COPY settings.json .local/share/code-server/User/settings.json
 # Use bash shell
 ENV SHELL=/bin/bash
 
+# Fix permissions
+RUN sudo chown -R coder:coder /home/coder/.local
+
 # Use our custom entrypoint script first
 COPY railway-entrypoint.sh /usr/bin/railway-entrypoint.sh
 ENTRYPOINT ["/usr/bin/railway-entrypoint.sh"]
--- railway-entrypoint.sh
+++ railway-entrypoint.sh
@@ -6,8 +6,5 @@ START_DIR=${1:-/home/coder/project}
 # Clone the git repo, if it exists
 [ -z "${GIT_REPO}" ] && echo "No GIT_REPO specified"; git clone $GIT_REPO $START_DIR
 
-# Fix permissions
-chown -R coder:coder /home/coder/.local
-
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
