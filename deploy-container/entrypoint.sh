4c4fa1a125f308d99780a2645a60eaad6d60cc38
Ben <me@bpmct.net>
2021-03-14 19:59:07 +0000

    reuse clone function

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -6,12 +6,17 @@ PREFIX="deploy-code-server"
 
 mkdir -p $START_DIR
 
+# function to clone the git repo or add a user's first file if no repo was specified.
+project_init () {
+    [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Example file. Have questions? Join us at https://community.coder.com" > $START_DIR/coder.txt | git clone $GIT_REPO $START_DIR
+}
+
 # add rclone config and start rclone, if supplied
 if [[ -z "${RCLONE_DATA}" ]]; then
     echo "[$PREFIX] RCLONE_DATA is not specified. Files will not persist"
 
-    # Clone the git repo, if it exists
-    [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" || git clone $GIT_REPO $START_DIR
+    # start the project
+    project_init()
 
 else
     echo "[$PREFIX] Copying rclone config..."
@@ -27,17 +32,18 @@ else
     chmod +x push_remote.sh pull_remote.sh
 
     if rclone ls $RCLONE_REMOTE_PATH; then
-        # grab the files from the remote instead of re-cloning the git repo
+        # grab the files from the remote instead of running project_init()
         echo "[$PREFIX] Pulling existing files from remote..."
         /home/coder/pull_remote.sh&
     else
         # we need to clone the git repo and sync
         echo "[$PREFIX] Pushing initial files to remote..."
-        [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Have questions? Join us at https://community.coder.com" > $START_DIR/file.txt; git clone $GIT_REPO $START_DIR
+        project_init()
         /home/coder/push_remote.sh&
     fi
 
 fi
 
+echo "[$PREFIX] Starting code-server..."
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
