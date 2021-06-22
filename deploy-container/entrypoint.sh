42a095cacf4c0ab6d81e6bbcfb7338a9dc36aa56
Ben Potter <me@bpmct.net>
2021-06-21 22:32:45 -0400

    add dotfiles support (#41)
    * initial dotfiles clone
    
    * add symlink
    
    * reverse dotfiles check
    
    * fix dir creation
    
    * touch up
    
    * document it

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -76,6 +76,25 @@ else
 
 fi
 
+# Add dotfiles, if set
+if [ -n "$DOTFILES_REPO" ]; then
+    # grab the files from the remote instead of running project_init()
+    echo "[$PREFIX] Cloning dotfiles..."
+    mkdir -p $HOME/dotfiles
+    git clone $DOTFILES_REPO $HOME/dotfiles
+
+    DOTFILES_SYMLINK="${RCLONE_AUTO_PULL:-true}"
+
+    # symlink repo to $HOME
+    if [ $DOTFILES_SYMLINK = "true" ]; then
+        shopt -s dotglob
+        ln -sf source_file $HOME/dotfiles/* $HOME
+    fi
+
+    # run install script, if it exists
+    [ -f "$HOME/dotfiles/install.sh" ] && $HOME/dotfiles/install.sh
+fi
+
 echo "[$PREFIX] Starting code-server..."
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
