f680570c763e355b803f1e0f2f9a51bf69e19d8d
Bruno <bruno@coder.com>
2021-08-23 17:48:41 +0000

    Fix async runner and add extra token info

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
-import { addDeployCommand } from './commands/deploy/addDeployCommand';
+import { program } from "commander";
+import { deployDigitalOcean } from "./deploys/deployDigitalOcean";
 
 const main = async () => {
-  program.version('0.0.1')
-  addDeployCommand(program)
-  await program.parseAsync(process.argv);
-}
+  program.version("0.1.0").description("deploy code-server to Digital Ocean");
+  program.parse();
+  await deployDigitalOcean();
+  process.exit(0);
+};
 
 main();
