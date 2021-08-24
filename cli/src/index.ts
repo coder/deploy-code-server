933ddcb3d74005fd3fc8ce9264a648e8739013ab
Bruno <bruno@coder.com>
2021-08-24 12:57:15 +0000

    Pull version from package.json

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,9 +2,12 @@
 
 import { program } from "commander";
 import { deployDigitalOcean } from "./deploys/deployDigitalOcean";
+import packageJson from "../package.json";
 
 const main = async () => {
-  program.version("0.1.0").description("deploy code-server to Digital Ocean");
+  program
+    .version(packageJson.version)
+    .description("deploy code-server to Digital Ocean");
   program.parse();
   await deployDigitalOcean();
   process.exit(0);
